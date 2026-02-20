a66b0e0c26a60e4c8696c5765967a6726f742106
marketthamer <ali.dawood1997@outlook.sa>
2026-02-20 03:53:14 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const data = {
             'شاي وقهوة': [
-                { id: 12, name: 'شاي أحمد تيباك', weight: 'صغير', price: 1250, img: 'imgs_tea/تيباك احمد صغير.jpg', stock: 1 },
+                { id: 12, name: 'شاي أحمد تيباك', weight: 'صغير', price: 1250, img: 'imgs_tea/تيباك احمد صغير.jpg', stock: 0 },
                 { id: 15, name: 'شاي ليبتون تيباك', weight: 'صغير', price: 1500, img: 'imgs_tea/تيباك ليبتون صغير.jpg', stock: 1 },
                 { id: 14, name: 'شاي محمود تيباك', weight: 'صغير', price: 1250, img: 'imgs_tea/تيباك محمود صغير.jpg', stock: 1 },
                 { id: 13, name: 'شاي حسن تيباك', weight: 'صغير', price: 1250, img: 'imgs_tea/تيباك حسن صغير.jpg', stock: 1 },
@@ -527,6 +527,7 @@ const data = {
 				],
             
         };
+
 
 
 
